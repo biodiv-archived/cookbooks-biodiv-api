2f6030024659ea117c919293ddd60bba13b0eda1
pearlsravanthi <sravanthi@strandls.com>
2018-05-03 20:14:30 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cookbooks-biodiv-api
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cookbooks-biodiv-api
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
