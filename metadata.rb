4320b731491c90cf0eb77cddbf796a85b5722562
pearlsravanthi <sravanthi@strandls.com>
2018-05-04 08:59:40 +0530

    changed name to biodiv-api

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'biodiv'
+name             'biodiv-api'
 maintainer       'Strand Life Sciences'
-maintainer_email 'sandeept@strandls.com'
+maintainer_email 'sravanthi@strandls.com'
 license          'Apache Licese, Version 2.0'
 description      'Installs/Configures biodiv-api'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
